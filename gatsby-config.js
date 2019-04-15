5d46a90c1f241c375d807c77d6290cf54bf2621e
Frank Ruiz <frank.ruiz.limom@gmail.com>
2019-04-15 16:53:17 -0700

    First commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Restaurant`,
+    description: `Example restaurant website built with Gatsby`,
+    author: `Frank`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
